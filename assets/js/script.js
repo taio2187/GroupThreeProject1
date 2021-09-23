f95a3be694b6368e21c0ae0f9b7c03145d0fbb30
taio <taio2187@gmail.com>
2021-09-22 19:53:08 -0700

    second fetch for api1

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ fetch('https://api.covid19api.com/countries')
 .then(function(response) {
     console.log(response);
 });
-fetch('https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z'
+fetch('https://api.covid19api.com/country/south-africa/status/confirmed?from=2021-03-11T00:00:00Z&to=2021-04-03T00:00:00Z'
 )
 .then(function(response2) {
     return response2.json();
